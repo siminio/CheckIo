4fab9fb7262a5ff2fec02e17171742ed50df1c6b
siminio <14363367+siminio@users.noreply.github.com>
2020-04-24 23:00:37 +0200

    Add GoesRightAfter solution in Scientific Expedition

---
+++ Scientific Expedition/GoesRightAfter.py
@@ -0,0 +1,16 @@
+def goes_after(word: str, first: str, second: str) -> bool:
+    return word.find(second) - word.find(first) == 1
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(goes_after('world', 'w', 'o'))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert goes_after('world', 'w', 'o') == True
+    assert goes_after('world', 'l', 'o') == False
+    assert goes_after('panorama', 'a', 'n') == True
+    assert goes_after('list', 'l', 'o') == False
+    assert goes_after('', 'l', 'o') == False
+    assert goes_after('list', 'l', 'l') == False
+    print("Coding complete? Click 'Check' to earn cool rewards!")
